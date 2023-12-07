311cec4042475408caa834ca74f167d2aed0a734
lyes hamroun <lyeshamroun@gmail.com>
2023-12-07 14:33:14 +0100

    modify escalidraw

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,39 @@ use args::args::Args;
 
 
 fn main() {
-    let t: u16 = 24;
+    /*
+    parsing 
+    
+    init env/memory
+    
+    while (pas encore tous les clients) {
+        envoie "bienvenue"
+        recv les reponses {
+            etablit la co
+            register un client
+        }
+    }
+    
+    prend le timestamp
+    
+    while (pas encore de ctrl+c || pas encore de gagnant) {
+        
+        est ce que j'ai recu un paquet {
+            est ce que le paquet est valide {
+                enregistre les datas de la commande recu dans le bon player
+                on craft le paquet gfx ??? 
+            }
+        }
+        
+        check si une des actions en attente a termine {
+            on craft le paquet gfx ???
+            on craft le paquet client
+            on envoie la reponse
+        }
 
-    println!("{:?}", Args::new(&t));
+        prend le timestamp + conversion timestamp vers tick
+
+        update les actions/ticks des actions en attentes
+    }
+    */
 }
